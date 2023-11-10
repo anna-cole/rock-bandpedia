44475c881e38115a03e4cf5471870d2373a2dae7
Anna Raptidou Cole <acrrj123@gmail.com>
2023-11-10 17:41:52 -0600

    Final version

--- src/pages/Concerts.js
+++ src/pages/Concerts.js
@@ -8,7 +8,7 @@ function Concerts() {
       </header>
       <h1 id="concert-text">Concerts</h1>
       <p id="concert-subtext">All upcoming concerts from the artists you’re tracking.</p>
-      <p className="concert-info">Saturday 11 November 2023<br></br>Guns N' Roses<br></br>Whisky A Go Go, Los Angeles, CA<br></br><br></br>Sunday 12 November 2023<br></br>Red Hot Chili Peppers<br></br>The Viper Room, Los Angeles, CA<br></br><br></br>Monday 13 November 2023<br></br>Metallica<br></br>Troubadour, Los Angeles, CA</p>
+      <p className="concert-info">Saturday, 11 November 2023<br></br>Guns N' Roses<br></br>Whisky A Go Go, Los Angeles, CA<br></br><br></br>Sunday, 12 November 2023<br></br>Red Hot Chili Peppers<br></br>The Viper Room, Los Angeles, CA<br></br><br></br>Monday, 13 November 2023<br></br>Metallica<br></br>Troubadour, Los Angeles, CA</p>
     </div>
   );
 };
