86f3b4337d25ddd2d42ab2213d59510b7f7341ea
Anna Raptidou Cole <acrrj123@gmail.com>
2023-11-10 17:40:33 -0600

    changed text about

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
       <header>
         <NavBar />
       </header>
-      <h1 id="about-text">Discover concerts for your favorite rock bands.</h1>
+      <h1 id="about-text">Discover concerts for your favorite rock bands!</h1>
       <p id="about-subtext">Rock Bandpedia is a free-content online encyclopedia about rock bands. Here you can find your favorite rock bands, their info and tour announcements.</p>
     </div>
   );
